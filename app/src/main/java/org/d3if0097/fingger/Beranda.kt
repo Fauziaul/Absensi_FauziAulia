6273f2897502b4977a56e43bdc07336ac607d7d6
Fauzi Aulia <auliafauzi835@gmail.com>
2023-04-01 22:54:45 +0700

    masuk ke tampilan home

--- app/src/main/java/org/d3if0097/fingger/Beranda.kt
+++ app/src/main/java/org/d3if0097/fingger/Beranda.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class Beranda : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.tampilan_masuk)
+        setContentView(R.layout.activity_beranda)
     }
 }
